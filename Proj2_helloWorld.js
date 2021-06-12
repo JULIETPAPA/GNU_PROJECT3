fdb86c8d6132bc62905919ccc280d07deae2e5ab
Jason Lee Prill <houdini4pres@gmail.com>
2021-06-11 22:00:54 -0400

    Add files via upload
    Be sure to have the correct port running on localhost Port:3000
    http://localhost:3000
    http://localhost:3000/about

--- Proj2_helloWorld.js
+++ Proj2_helloWorld.js
@@ -1,23 +1,42 @@
 var http = require('http');
+        fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+        if(!responseCode) responseCode = 200;
+        fs.readFile(__dirname + path, function(err,data) {
+            if(err) {
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('500 - Internal Error');
+            } else {
+                    res.writeHead(responseCode,
+                                { 'Content-Type': contentType });
+                    res.end(data);
+            }
+            
+        });
+}
 
 http.createServer(function(req,res){
-        //normalize url by removing querystring, optional
-        // trailing slash, and making it lowercase
-        var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+        // normalize url by removing querystring, optional
+        // trailing slash, and making lowercase
+        var path = req.url.replace(/\/?(?:\?.*)?$/, '')
+                .toLowerCase();
         switch(path) {
                 case '':
-                        res.writeHead(200, { 'Content-Type': 'text/plain' });
-                        res.end('Homepage');
-                        break;
+                    serveStaticFile(res, '/public/home.html', 'text/html');
+                    break;
                 case '/about':
-                        res.writeHead(200, { 'Content-Type': 'text/plain' });
-                        res.end('About');
-                        break;
+                    serveStaticFile(res, '/public/about.html', 'text/html');
+                    break;
+                case 'img/logo.jpg':
+                    serveStaticFile(res, '/public/img/logo.jpg',
+                            'image/jpeg');
+                    break;
                 default:
-                        res.writeHead(404, { 'Content-Type': 'text/plain' });
-                        res.end('Not Found');
-                break;
+                    serveStaticFile(res, '/public/404.html', 'text/html',
+                            404);
+                    break;
         }
 }).listen(3000);
 
-console.log('Servers started on localhost:3000; press Ctrl-C to terminate ;) ');
+console.log('Server started on local hostes: 3000; press Ctrl-C to terminate....');
